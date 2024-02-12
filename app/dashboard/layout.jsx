9816fcdcd6ba96daf45888b869f58bdac322f408
Samet Mavzer <samet.mavzer@intern.priviasecurity.com>
2024-02-12 18:02:09 +0300

    users and product pages created

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Sidebar from '../ui/dashboard/sidebar/sidebar'
-import Navbar from '../ui/dashboard/navbar/navbar'
+import Navbar from "../ui/dashboard/navbar/navbar"
+import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import styles from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
 const Layout = ({ children }) => {
     return (
@@ -12,8 +12,8 @@ const Layout = ({ children }) => {
             <div className={styles.content}>
                 <Navbar />
                 {children}
+                <Footer />
             </div>
-
         </div>
     )
 }
